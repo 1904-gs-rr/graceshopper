557ff1d7a61f332b1ffd69087ba32fafff27ab0d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-06-11 11:32:23 -0400

    created single user route by id

--- server/api/users.js
+++ server/api/users.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+/*GET SINGLE USER BY ID*/
+
+router.get('/:id', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+    const {data} = await User.findById(req.params.id)
+    if (!user) {
+      res.status(404).send()
+    } else {
+      res.json(data)
+    }
+  } catch (error) {
+    next(error)
   }
 })
