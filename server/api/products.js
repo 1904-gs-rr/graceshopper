405d11052fed47b55b632776744f77b5af87cc96
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-06-11 11:55:04 -0400

    added products.js file, get single and all products routes

---
+++ server/api/products.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+module.exports = router
+
+/**GET ALL PRODUCTS**/
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.status(200).json(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+/**GET SINGLE PRODUCT**/
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    res.status(200).json(products)
+  } catch (error) {
+    next(error)
+  }
+})
