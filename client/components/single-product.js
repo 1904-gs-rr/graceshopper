7f7275e42a94c5c86f161d42cbe83ce4afcb9536
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-06-12 16:03:39 -0400

    added add to cart button functionality on single product view

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
+import {addingItem} from '../store/cart'
 
-export default class SingleProduct extends React.Component {
+class SingleProduct extends React.Component {
   constructor() {
     super()
     this.state = {
@@ -22,8 +23,18 @@ export default class SingleProduct extends React.Component {
         <h1>{product.name}</h1>
         <img src={product.imageUrl} />
         <h2>Quantity: {product.quantity}</h2>
-        <button>Add to cart</button>
+        <button type="button" onClick={() => this.props.addToCart(product)}>
+          Add to cart
+        </button>
       </div>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addToCart: item => dispatch(addingItem(item))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SingleProduct)
