4e0c373df730c553c8f1e0ece6a1ca56ca1912eb
Wilson Leung <wilsonleung32@gmail.com>
2019-06-13 15:27:05 -0400

    added quantity to through table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,19 @@
 const User = require('./user')
 const Product = require('./product')
 const Order = require('./order')
-
+const db = require('../db')
+const Sequelize = require('sequelize')
 User.hasMany(Order)
 // Order.hasOne(User)
+const CartProduct = db.define('cart_product', {
+  cartQuantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1
+  }
+})
 
-Order.belongsToMany(Product, {through: 'product_order'})
-Product.belongsToMany(Order, {through: 'product_order'})
+Order.belongsToMany(Product, {through: CartProduct})
+Product.belongsToMany(Order, {through: CartProduct})
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -20,7 +27,9 @@ Product.belongsToMany(Order, {through: 'product_order'})
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
 module.exports = {
+  CartProduct,
   User,
   Product,
   Order
