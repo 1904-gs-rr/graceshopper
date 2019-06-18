480eae5273e9a64fdbd6ade37f7334b7c06ba33a
Jorge Acosta <jh.acostadelapena@gmail.com>
2019-06-18 16:13:43 -0400

    user model and script updated

--- client/components/updateUser.js
+++ client/components/updateUser.js
@@ -5,13 +5,12 @@ import {updateUsers} from '../store/user'
 class UserUpdateForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {email: '', isAdmin: false}
+    this.state = {firstName: '', lastName: '', email: ''}
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(evt) {
     evt.preventDefault()
-
     this.props.updateUsers(this.state, this.props.match.params.id)
   }
 
@@ -20,21 +19,41 @@ class UserUpdateForm extends React.Component {
       <div>
         <form className="form" onSubmit={this.handleSubmit}>
           <ul>
-            Update Info:
-            <label>Email: </label>
-            <input
-              name="eMail"
-              type="text"
-              required="true"
-              email="true"
-              onChange={evt => this.setState({email: evt.target.value})}
-            />
-            <label>is Admin: </label>
+            Update Your Contact Info!
+            <div>
+              <label> First Name: </label>
+              <input
+                name="first"
+                type="text"
+                onChange={evt => this.setState({first: evt.target.value})}
+              />
+              <label>Last Name: </label>
+              <input
+                name="last"
+                type="text"
+                onChange={evt => this.setState({last: evt.target.value})}
+              />
+              <label>Email: </label>
+              <input
+                name="eMail"
+                type="email"
+                email="true"
+                onChange={evt => this.setState({email: evt.target.value})}
+              />
+              {/* <label>Password: </label> */}
+              {/* <input
+                name="password"
+                type="password"
+                required="true"
+                onChange={evt => this.setState({password: evt.target.value})}
+              /> */}
+              {/* <label>is Admin: </label>
             <input
               name="isAdmin"
               type="boolean"
               onChange={evt => this.setState({isAdmin: evt.target.value})}
-            />
+            /> */}
+            </div>
             <button
               type="submit"
               onClick={evt => {
